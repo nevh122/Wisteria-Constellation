439d3517a3d48290e48ec6b89d4500905bb4a34c
nvhuet <nvhuet@student.apc.edu.ph>
2022-04-27 02:25:21 +0800

    Environmental and NPC Butterfly effect

--- Constellations/Assets/Scripts/Luna1stEnc.cs
+++ Constellations/Assets/Scripts/Luna1stEnc.cs
@@ -2,43 +2,66 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using UnityEngine.Tilemaps;
 
 public class Luna1stEnc : MonoBehaviour
 {
     public Text maintxt;
     public SpriteRenderer LunaMain;
-    public Sprite LunaHappy;
-    public Sprite LunaAngry;
-    public GameObject Button1;
-    public GameObject Button2;
-    public bool isAngry;
+    public Sprite LunaHappy, LunaAngry;
+    public GameObject Button1, Button2;
+    public int isAngry = 0;
 
+    public Tile Flowers, Reed;
+    public Vector3Int[] tilePos;
+    public Tilemap ABgrnd;
     public void YesAnswer()
     {
+        maintxt.alignment = TextAnchor.MiddleLeft;
         maintxt.text = "YEEYY! I knew you can do it!";
         LunaMain.sprite = LunaHappy;
         Button1.SetActive(false);
         Button2.SetActive(false);
-        isAngry = false;
+        isAngry = 1;
     }
     public void NoAnswer()
     {
+        maintxt.alignment = TextAnchor.MiddleLeft;
         maintxt.text = "HUH?! But what happens to the game if you dont finish it?";
         LunaMain.sprite = LunaAngry;
         Button1.SetActive(false);
         Button2.SetActive(false);
-        isAngry = true;
-
+        isAngry = 2;
     }
     public void OnClose()
     {
-        if (isAngry == true)
+        switch (isAngry)
         {
-            maintxt.text = "Dont worry about ill just find someone else to do it.";
+            case 2:
+                maintxt.text = "Dont worry about ill just find someone else to do it.";
+                break;
+            case 1:
+                maintxt.text = "I knew I can count on you! I believe in you!";
+                break;
+            default:
+                break;
         }
-        else if(isAngry == false)
+    }
+    public void TileColorLuna()
+    {
+        for (int i = 0; i < tilePos.Length; i++)
         {
-            maintxt.text = "I knew I can count on you! I believe in you!";
+            switch (isAngry)
+            {
+                case 2:
+                    ABgrnd.SetTile(tilePos[i], Reed);
+                    break;
+                case 1:
+                    ABgrnd.SetTile(tilePos[i], Flowers);
+                    break;
+                default:
+                    break;
+            }
         }
     }
 }
