e86cbb33a0c9a591e1ac3e00956ef644f245edc1
nvhuet <nedhuet123@gmail.com>
2022-10-18 03:28:15 +0800

    Movement and Transition Experiments Added
    - Movement scripts came from the previous project
    - Transitions added for room revealing, adding a sense of whats on the other side of the area

---
+++ Constellations-Prototype/Assets/Scripts/Player/PlayerController.cs
@@ -0,0 +1,40 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    [SerializeField] float moveSpeed; 
+    void Start()
+    {
+
+    }
+
+    void Update()
+    {
+        MovePlayer();
+    }
+    
+    private void MovePlayer()
+    {
+        Vector2 direction = Vector2.zero;
+        if (Input.GetKey(KeyCode.A) || Input.GetKey(KeyCode.LeftArrow))
+        {
+            direction.x = -1;
+        }
+        else if (Input.GetKey(KeyCode.D) || Input.GetKey(KeyCode.RightArrow))
+        {
+            direction.x = 1;
+        }
+        if (Input.GetKey(KeyCode.W) || Input.GetKey(KeyCode.UpArrow))
+        {
+            direction.y = 1;
+        }
+        else if (Input.GetKey(KeyCode.S) || Input.GetKey(KeyCode.DownArrow))
+        {
+            direction.y = -1;
+        }
+        direction.Normalize();
+        GetComponent<Rigidbody2D>().velocity = moveSpeed * direction;
+    }
+}
