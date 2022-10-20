10bff444f279b63a6e19f0f1998bf457ce73129c
nvhuet <nedhuet123@gmail.com>
2022-10-20 21:02:51 +0800

    Added Postprocessing and Added some animations
    - Added post processing for vignette effect
    * add way to make lines straight and not jagged
    - Added animation and controllers for clover leaf and player

--- Constellations-Prototype/Assets/Scripts/Player/PlayerController.cs
+++ Constellations-Prototype/Assets/Scripts/Player/PlayerController.cs
@@ -4,7 +4,8 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
-    [SerializeField] float moveSpeed; 
+    [SerializeField] float moveSpeed;
+    public Animator playerAnim;
 
     void Update()
     {
@@ -12,27 +13,34 @@ public class PlayerController : MonoBehaviour
     }
     
 
-    //Handles Player movement usign WASD or the Arrow Keys
-    private void MovePlayer()
+    //Handles Player movement using WASD or the Arrow Keys with animations
+    public void MovePlayer()
     {
         Vector2 direction = Vector2.zero;
         if (Input.GetKey(KeyCode.A) || Input.GetKey(KeyCode.LeftArrow))
         {
             direction.x = -1;
+            playerAnim.SetInteger("Direction", 3);
         }
         else if (Input.GetKey(KeyCode.D) || Input.GetKey(KeyCode.RightArrow))
         {
             direction.x = 1;
+            playerAnim.SetInteger("Direction", 4);
         }
         if (Input.GetKey(KeyCode.W) || Input.GetKey(KeyCode.UpArrow))
         {
             direction.y = 1;
+            playerAnim.SetInteger("Direction", 2);
         }
         else if (Input.GetKey(KeyCode.S) || Input.GetKey(KeyCode.DownArrow))
         {
             direction.y = -1;
+            playerAnim.SetInteger("Direction", 1);
+
         }
+
         direction.Normalize();
+        playerAnim.SetBool("IsMoving", direction.magnitude > 0);
         GetComponent<Rigidbody2D>().velocity = moveSpeed * direction;
     }
 }
